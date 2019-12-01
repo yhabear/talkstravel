c33b06c074f0fcccc0c2a30ecdb309a14cb68e26
yayha <yhafar066@hotmail.com>
2019-12-02 01:17:58 +0700

    update navbar and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'talkstravel';
+  constructor() { }
+
+  ngOnInit() {
+    $(document).ready(function () {
+      var navbar = $('.navbar');
+      var origOffsetY = navbar.offset().top;
+      function scroll() {
+          if ($(window).scrollTop() >= origOffsetY) {
+              $('.navbar').addClass('sticky');
+              $('.container').addClass('navbar-padding');
+          } else {
+              $('.navbar').removeClass('sticky');
+              $('.container').removeClass('navbar-padding');
+          }
+      }
+      document.onscroll = scroll;
+  });
+  }
+
 }
