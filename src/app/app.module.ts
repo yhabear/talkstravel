ca9a1819987efbeb4d98e3c009e97f87870f0bbf
KEDTDM <tidamasphaiddee937@gmail.com>
2019-12-05 00:34:31 +0700

    kedked
    components login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
+// Routes
+export const router: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'components/login', component: LoginComponent },
+  
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
